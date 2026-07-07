81d49a50800d4a223245b87a3768055990748ad3
rex-del <150361329+rex-del@users.noreply.github.com>
2026-07-08 01:51:56 +0300

    Initial commit: Potshelf

---
+++ iOS/Potshelf/App.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+@main
+struct PotshelfApp: App {
+    @StateObject private var store = Store()
+    @StateObject private var purchases = PurchaseManager()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(store)
+                .environmentObject(purchases)
+                .preferredColorScheme(.light)
+        }
+    }
+}
